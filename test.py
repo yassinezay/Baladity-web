983bde9bd5d5fbb6b71f3b4e315c9f0ca56aa715
yassinezay <yassine.zayane@esprit.tn>
2024-06-15 14:00:47 +0100

    feat update

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-r = requests.post(
-    "https://api.deepai.org/api/text2img",
-    data={
-        'text': 'hey',
-    },
-    headers={'api-key': '9d8b806f-21d1-4e85-8c64-cdcafb2fafed'}
-)
-print(r.json())
